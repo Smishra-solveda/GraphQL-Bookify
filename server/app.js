6117b3b0724dc974525841a63b0f57801fef6f12
Shashwat Mishra <smishra@solveda.com>
2024-02-01 23:30:53 +0530

    Backend Implemented

---
+++ server/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+const app = express();
+
+dotenv.config({
+    path: './.env'
+})
+
+// allow cross-origin requests
+app.use(cors());
+
+const password = process.env.PASSWORD;
+mongoose.connect(`mongodb+srv://shashwat:11223344@cluster0.8e6yml0.mongodb.net/NinjaGraphQL`)
+mongoose.connection.once('open', () => {
+    console.log('conneted to database');
+});
+
+// bind express with graphql
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
+app.listen(4000, () => {
+    console.log('now listening for requests on port 4000');
+});
